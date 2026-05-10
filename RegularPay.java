9b2780b6d57628db5d148d10de551c3154a0ec1b
Sara Sunny <110349622+ssunny264@users.noreply.github.com>
2026-05-10 13:14:47 -0400

    Add RegularPay class for pay calculation

---
+++ RegularPay.java
@@ -0,0 +1,15 @@
+/*
+ * Sara Sunny
+ */
+package sunny_lab8_prob2;
+
+/**
+ *
+ * @author slbru
+ */
+public class RegularPay extends PayCalculator{
+        
+    public RegularPay(double payRate){
+        this.payRate = payRate;
+    }
+}
